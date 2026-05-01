e41ed2d187d464cf412d960e9e506dcaa1a97131
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-01 14:31:05 -0700

    Implemented basic fetch API to get GitHub user data

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const usernameInput=document.getElementById("username");
 const searchBtn=document.getElementById("searchBtn");
 const result=document.getElementById("result");
+
+searchBtn.addEventListener("click",async()=>{
+    const username=usernameInput.Value;
+    
+    const res=await fetch(`https://api.github.com/users/${username}`);
+    const data=await res.json();
+
+    console.log(data);
+})
