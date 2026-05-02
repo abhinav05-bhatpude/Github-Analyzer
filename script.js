3116b0bce84aa0c8d615d4d1b2d57bc1d8d0c914
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-02 14:08:49 -0700

    Displayed additional user information including repos and following count

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const usernameInput=document.getElementById("username");
-const searchBtn=document.getElementById("searchBtn");
-const result=document.getElementById("result");
+const usernameInput = document.getElementById("username");
+const searchBtn = document.getElementById("searchBtn");
+const result = document.getElementById("result");
 
-searchBtn.addEventListener("click",async()=>{
-    const username=usernameInput.Value;
-    
-    const res=await fetch(`https://api.github.com/users/${username}`);
-    const data=await res.json();
+searchBtn.addEventListener("click", async () => {
+  const username = usernameInput.value;
 
-    console.log(data);
-})
+  const res = await fetch(`https://api.github.com/users/${username}`);
+  const data = await res.json();
+
+  console.log(data);
+
+  result.innerHTML=`
+  <img src="${data.avatar_url}" width="90">
+  <h3>${data.name || data.login}</h3>
+  <p>${data.bio || "No bio available"}</p>
+  <p>Followers : ${data.followers}</p>
+  <p>Following: ${data.following}</p>
+  <p>Public Repos : ${data.public_repos}</p>
+  `;
+});
