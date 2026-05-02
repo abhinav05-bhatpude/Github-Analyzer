b35338786d1dcb5564834b35c7f4b43bf710d7e9
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-02 14:27:05 -0700

    Improved loading state with better UI feedback

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ const result = document.getElementById("result");
 searchBtn.addEventListener("click", async () => {
   const username = usernameInput.value;
 
+  result.innerHTML="<p>Loading user data...</p>"
+
   const res = await fetch(`https://api.github.com/users/${username}`);
   const data = await res.json();
 
   console.log(data);
 
   result.innerHTML=`
+ 
   <img src="${data.avatar_url}" width="90">
   <h3>${data.name || data.login}</h3>
   <p>${data.bio || "No bio available"}</p>
